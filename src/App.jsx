345fe3d9d88d0c39a3da67df8f2b15212685189f
Reshma-Ghosh <reshmaghosh114@gmail.com>
2023-04-27 00:52:19 +0200

    React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import image1 from "./images/skincare.avif";
+import logo from "./images/logo.png";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">App here</div>;
+  return (
+    <div className="App">
+      <div className="bg"></div>
+
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav class="nav">
+      <div class="nav-logo">
+        <img src="logo.png" alt="logo" />
+      </div>
+      <div class="nav-item">
+        <ul>
+          <li>
+            <a href="##">Home</a>
+          </li>
+          <li>
+            <a href="##">Product</a>
+          </li>
+          <li>
+            <a href="##">Favourite</a>
+          </li>
+          <li>
+            <a href="##">Chat</a>
+          </li>
+        </ul>
+      </div>
+      <div class="text">
+        <h1>Sale 20% Off</h1>
+        <h3>On Everything</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque sed
+          <br></br>
+          corporis distinctio provident non accusamus quidem iusto at quaerat
+          sint?
+        </p>
+        <button>Shop Now</button>
+      </div>
+    </nav>
+  );
 }
